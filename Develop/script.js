8649f875f1a06fb6735d7f5f7970c19a147c4849
funkcake <castobrianj@gmail.com>
2021-03-07 13:36:06 -0600

    final
    completed

---
+++ Develop/script.js
@@ -0,0 +1,40 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(l) {
+  var criteriaArry = ["", "", "", ""];
+  criteriaArry[0] = prompt("Would you like to include uppercase letters? (Y/N)");
+  criteriaArry[1] = prompt("Would you like to include lowercase letters? (Y/N)");
+  criteriaArry[2] = prompt("Would you like to include numbers? (Y/N)");
+  criteriaArry[3] = prompt("would you like to include symbols? (Y/N)");
+  var charArry = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%^&*?"];
+  var char_list = "";
+  var text = "";
+  for (var i = 0; i < criteriaArry.length; i++) {
+    if (criteriaArry[i] == "Y") {
+      char_list += charArry[i];
+      console.log(char_list);
+    }
+  }
+  //var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?";
+  for (var i = 0; i < l; i++) {
+    text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  return text;
+}
+
+function writePassword() {
+  var length = prompt("How long do you want your password? (Please enter a number.)");
+
+  var password = generatePassword(length);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
